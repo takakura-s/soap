b0e3c6888f98f320f4bd4747fd401fddf200e24b
ebook-takakura <s.takakura@ebookjapan.net>
2018-01-19 13:44:36 +0900

    c1

---
+++ src/main/java/https/www_howtodoinjava_com/xml/school/package-info.java
@@ -0,0 +1,9 @@
+//
+// このファイルは、JavaTM Architecture for XML Binding(JAXB) Reference Implementation、v2.2.7によって生成されました 
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>を参照してください 
+// ソース・スキーマの再コンパイル時にこのファイルの変更は失われます。 
+// 生成日: 2017.12.01 時間 06:17:41 PM JST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "https://www.howtodoinjava.com/xml/school", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package https.www_howtodoinjava_com.xml.school;
